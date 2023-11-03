31738c28adf934d01d4b6eeeb937a3855a8937b2
Jacky Chen <jackyc4200@gmail.com>
2023-11-03 02:24:10 -0400

    first commit

---
+++ Card.cpp
@@ -0,0 +1,66 @@
+#include "Card.hpp"
+
+Card::Card(const Card& rhs) {
+	this->instruction_ = rhs.instruction_;
+	this->cardType_ = rhs.cardType_;
+}
+
+Card::Card() {
+	this->instruction_ = "";
+	this->cardType_ = POINT_CARD;
+}
+
+Card::~Card() {
+	std::cout << "Destructor Executed" << std::endl;
+}
+
+Card& Card::operator=(const Card& rhs) {
+	if (this != &rhs) {
+		this->instruction_ = rhs.instruction_;
+		this->cardType_ = rhs.cardType_;
+	}
+	return *this;
+}
+
+Card::Card(Card&& rhs) {
+	this->cardType_ = rhs.cardType_;
+	this->instruction_ = rhs.instruction_;
+}
+
+Card& Card::operator=(Card&& rhs) {
+	std::swap(instruction_, rhs.instruction_);
+	std::swap(cardType_, rhs.cardType_);
+	return *this;
+}
+
+std::string Card::getType() const {
+	if (this->cardType_ == POINT_CARD) {
+		return "Point Card";
+	}
+	else if (this->cardType_ == ACTION_CARD) {
+		return "Action Card";
+	}
+	else {
+		return "No Type";
+	}
+}
+
+const std::string& Card::getInstruction() const {
+	return this->instruction_;
+}
+
+void Card::setType(const CardType& type) {
+	this->cardType_ = type;
+}
+
+void Card::setInstruction(const std::string& instruction) {
+	this->instruction_ = instruction;
+}
+
+void Card::setDrawn(const bool& drawn) {
+	this->drawn_ = drawn;
+}
+
+bool Card::getDrawn() const {
+	return this->drawn_;
+}
